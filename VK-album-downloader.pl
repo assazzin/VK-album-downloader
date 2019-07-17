7d61aab8890384e87e765fc8b5912728966c4cc2
assazzin <niszazsa@msn.com>
2019-07-17 21:18:34 +0700

    Create VK-album-downloader.pl

---
+++ VK-album-downloader.pl
@@ -0,0 +1,87 @@
+use LWP::UserAgent;
+
+$url = shift || die " [+] Usage: perl $0 [URL]\n";
+unless($url=~/^https?:\/\/vk\.com\//) {
+	die " [!] The URL prefix should start with \"https://vk.com/\"\n";
+}
+
+($albumName) = $url =~ /.*\/(.*)$/;
+$previewFolder = $albumName . "-preview";
+$imageFolder = $albumName . "-image";
+
+print " [+] VK album downloader\n";
+print " [+] Download only preview [y/N]: ";
+chomp($input=<STDIN>);
+$isPreview = uc($input) eq 'Y' ? "true" : "false";
+print " [+] Checking media files on url: $url\n";
+
+$agent=LWP::UserAgent->new();
+
+if($isPreview eq "true") {
+	mkdir($previewFolder);
+}
+
+$offset = 0;
+do {
+	$checkLastOffset = $offset;
+	$content=$agent->post($url,[
+		"al" => "1",
+		"offset" => $offset,
+		"part" => "1"
+	])->content;
+
+	while($content=~/background-image: url\((.*?)\)/g) {
+		$imageURL = $1;
+		push(@imageURLs, $imageURL);
+		$offset++;
+	}
+} until($checkLastOffset == $offset);
+
+print " [+] Found $offset images.\n";
+sleep(1);
+
+if($isPreview eq "true") {
+	print " [+] Downloading preview images...\n";
+	foreach $imageURL (@imageURLs) {
+		($imageName) = $imageURL =~ /.*\/(.*)$/;
+		print "     => $imageURL\n";
+		download($imageURL,$previewFolder."/".$imageName);
+	}
+	exit;
+}
+
+mkdir($imageFolder);
+$imageCount = $offset;
+$offset = 0;
+
+print " [+] Downloading full images...\n";
+do {
+	$content=$agent->post('https://vk.com/al_photos.php',[
+		"act" => "show",
+		"al" => "1",
+		"direction" => "1",
+		"list" => $albumName,
+		"offset" => $offset
+	])->content;
+	@contentArray = split(/"id":/, $content);
+	foreach $content (@contentArray) {
+		if($content=~/^"(.*?)".*_src":"(.*?)"/g) {
+			$id = $1;
+			$imageURL = $2;
+			$imageURL=~s/\\\//\//g;
+			($imageName) = $imageURL =~ /.*\/(.*)$/;
+			$offset++;
+			print "     => [$offset/$imageCount][ID:$id] $imageURL\n";
+			download($imageURL,$imageFolder."/".$imageName);
+			last if($offset >= $imageCount);
+		}
+	}
+} until($offset >= $imageCount);
+
+sub download {
+	my $download = $agent->get($_[0])->content;
+	open IMG, ">$_[1]";
+	binmode IMG;
+	print IMG $download;
+	close IMG;
+}
